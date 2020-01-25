d42dfd55c1f300e01f15897c692ac8c748bcecd5
Abhi <abhiraja1012@gmail.com>
2020-01-24 23:57:19 -0500

    Add recording of udp data

--- ByteWriter/F1_Capture/Main_Program.cs
+++ ByteWriter/F1_Capture/Main_Program.cs
@@ -4,7 +4,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 
-// Adapted from the following:
+// Adapted from below link, to record udp data for playback later. 
 //http://www.robertgray.net.au/posts/2012/3/connecting-to-codemasters-telemetry-feed
 //https://github.com/robgray/F1Speed
 
@@ -18,7 +18,7 @@ namespace F1_Capture
 
         // This is the IP endpoint we are connecting to (i.e. the IP Address and Port F1 2012 is sending to)      
         static private IPEndPoint remoteIP;
-        // This is the IP endpoint for capturing who actually sent the data
+        //read datagrams sent from any source.
         static private IPEndPoint senderIP = new IPEndPoint(IPAddress.Any, 0);
         // UDP Socket for the connection
         static private UdpClient udpSocket;
@@ -27,13 +27,11 @@ namespace F1_Capture
 
         // Mutex used to protect latestData from simultaneous access by both threads
         static Mutex syncMutex = new Mutex();
-        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Listing on port " + PORTNUM + " for connections from" +IP);
-
+            Console.WriteLine("Listening on port " + PORTNUM + " for connections from" + IP);
             remoteIP = new IPEndPoint(IPAddress.Parse(IP), PORTNUM);
-
             // Set up the socket for collecting game telemetry
             try
             {
@@ -45,32 +43,73 @@ namespace F1_Capture
             }
             catch (Exception error)
             {
-               Console.WriteLine(error.ToString());
+                Console.WriteLine(error.ToString());
             }
+            int mode = 1; // TODO: get mode from command line
 
-            FetchData();    
+            if (mode == 0)
+            {
+                RecordPackets();
+            }
+            else if (mode == 1)
+            {
+                PlaybackPackets();
+            }
+            else if (mode == 2)
+            {
+                ConvertLivePackets();
+            }
         }
-        // This method runs continously in the data collection thread.  It
-        // waits to receive UDP packets from the game, and then writes to a bin file. 
-        static private void FetchData()
+
+        /// <summary>
+        /// Records UDP packets, and saves it to seperate .bin files
+        /// </summary>
+        static private void RecordPackets()
         {
             Console.WriteLine("Waiting for packets");
             int n = 0;
-
             while (true)
             {
-                // Get the data (this will block until we get a packet)
-                Byte[] receiveBytes = new byte[2];
-                receiveBytes[0] = 2; receiveBytes[1] = 3;//udpSocket.Receive(ref senderIP);
+                Byte[] receiveBytes = udpSocket.Receive(ref senderIP);
                 syncMutex.WaitOne();
-                string filename = "Packet" + n+".bin";
+                string filename = "Packet" + n + ".bin";
                 File.WriteAllBytes(filename, receiveBytes);
                 n++;
                 syncMutex.ReleaseMutex();
+            }
+        }
 
+        /// <summary>
+        /// Plays back Binary files in the current directory
+        /// </summary>
+        static private void PlaybackPackets()
+        {
+            string path = "D:\\Packets";
+
+            syncMutex.WaitOne();
+
+            int count = Directory.GetFiles(path, "*").Length; Console.WriteLine(count);
+            Console.ReadLine();
+            for (int i = 0; i < count; i++)
+            {
+                string filename = "D:\\Packets\\Packet" + i + ".bin";
+                Byte[] udpBytes = File.ReadAllBytes(filename);
+                //TODO: publish packets onto port
+
+            }
+            syncMutex.ReleaseMutex();
+        }
+
+        static private void ConvertLivePackets()
+        {
+            Console.WriteLine("Waiting for packets");
+            while (true)
+            {
+                // Get the data (this will block until we get a packet)
+                Byte[] receiveBytes = udpSocket.Receive(ref senderIP);
                 // Convert the bytes received to the shared struct
-                //latestData = PacketUtilities.ConvertToPacket(receiveBytes);
-                //Console.WriteLine("Time:"+ latestData.Time+"   "+"Speed: "+latestData.Speed*2.23694);
+                latestData = PacketUtilities.ConvertToPacket(receiveBytes);
+                Console.WriteLine("Time:" + latestData.Time + "   " + "Speed: " + latestData.Speed * 2.23694);
             }
         }
     }
