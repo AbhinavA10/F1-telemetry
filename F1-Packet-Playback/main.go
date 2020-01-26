74aa5fbbc86ee1c9e55be4e2935382b1df23542b
Abhinav <abhiraja1012@gmail.com>
2020-01-26 00:23:37 -0500

    add udp write

--- F1-Packet-Playback/main.go
+++ F1-Packet-Playback/main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	const IP string = "127.0.0.1"
-	const PORT int = 20777
+	const PORT string = "20777"
 	packetPlayer := playback.NewPacketPlayer(IP, PORT)
 	playback.PlayPackets(packetPlayer)
 }
--- F1-Packet-Playback/playback/playback.go
+++ F1-Packet-Playback/playback/playback.go
@@ -12,15 +12,24 @@ import (
 
 //PacketPlayer holds udp playback settings
 type PacketPlayer struct {
-	ip    string
-	port  int
-	mutex sync.Mutex
-	//TODO: define udp socket
+	connection *net.UDPConn
+	mutex      sync.Mutex
 }
 
 // NewPacketPlayer instantiates and returns a new PacketPlayer with supplied config
-func NewPacketPlayer(IP string, PORT int) *PacketPlayer {
-	player := PacketPlayer{ip: IP, port: PORT}
+func NewPacketPlayer(IP string, PORT string) *PacketPlayer {
+	player := PacketPlayer{}
+	udpAddr, err := net.ResolveUDPAddr("udp", IP+":"+PORT)
+	if err != nil {
+		fmt.Println(err)
+	}
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	if err != nil {
+		fmt.Println(err)
+	}
+	player.connection = conn
+	// Source of UDP publishing will be conn.LocalAddr().String()
+	// We are publishing to conn.RemoteAddr().String()
 	return &player
 }
 
@@ -31,6 +40,7 @@ func getNumFiles(path string) int {
 
 // PlayPackets reads bytes from a folder and publishes to udp port
 func PlayPackets(player *PacketPlayer) {
+	defer player.connection.Close()
 	const BaseFilePath string = "/media/abhi_ubuntu18/600C7EC53DCD4841/f1/Packets"
 	var n int
 	var filepath string
@@ -41,12 +51,16 @@ func PlayPackets(player *PacketPlayer) {
 		player.mutex.Lock()
 		filepath = BaseFilePath + "/Packet" + strconv.Itoa(n) + ".bin"
 		udpData := ReadData(filepath)
-		PublishPacket(udpData)
+		PublishPacket(player, udpData)
 		player.mutex.Unlock()
 	}
 }
 
 // PublishPacket publishes supplied data onto a udp packet
-func PublishPacket(data []byte) {
-	//TODO: do udp things
+func PublishPacket(player *PacketPlayer, data []byte) {
+	_, err := player.connection.Write(data)
+	//TODO: am getting connection refused
+	if err != nil {
+		fmt.Println(err)
+	}
 }
