92bba2ab85b4e52f52a03d5074597ed10e978443
Abhinav <abhiraja1012@gmail.com>
2020-01-26 21:09:05 -0500

    make some notes

--- F1-Telemetry/go.mod
+++ F1-Telemetry/go.mod
@@ -1,3 +1,5 @@
 module F1-2012-telemetry
 
 go 1.13
+
+require github.com/influxdata/influxdb1-client v0.0.0-20191209144304-8bf82d3c094d
--- F1-Telemetry/influxDB/influxSender.go
+++ F1-Telemetry/influxDB/influxSender.go
@@ -1,6 +1,26 @@
 package influxdata
 
-//https://github.com/influxdata/influxdb/blob/1.8/client/v2/example_test.go
-//https://github.com/influxdata/influxdb/blob/1.8/client/v2/client.go
+// To add a new point to influxdb, I will need to convert the F1Packet struct to a hashmap
 
-// To add a new point to influxdb, I will need to convert the pack struct to a hashmap
+/*A channel is safe for concurrent access.
+select and case is used to wait for different channels
+can send data over channels
+
+Read files into a queue in one 'thread'
+Publish on udp port on another 'thread', by removing from front of queue
+To be able to use queue, use "container/list" which is a doubly linked list
+need to call
+var list  *List
+dataToPub := list.Front() // of type Element
+list.Remove(dataToPub)
+
+*/
+
+import (
+	_ "github.com/influxdata/influxdb1-client" // this is important because of the bug in go mod
+	client "github.com/influxdata/influxdb1-client/v2"
+)
+
+func ConnectClient() {
+	c, err := client.NewHTTPClient()
+}
